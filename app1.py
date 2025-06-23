929d19c7cb49cd18d52148a9dc86deceb07983eb
ivzvrr <ivancaxxx@gmail.com>
2025-06-23 13:16:03 +0500

    Добавил новую функциональность для презентаций

--- app1.py
+++ app1.py
@@ -7,17 +7,10 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import aiohttp
 from aiogram import types
 
-# Создаем БД (если её нет)
+
+# Подключаем бд
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS allowed_users (
-        user_id INTEGER PRIMARY KEY,
-        username TEXT
-    )
-""")
-conn.commit()
-
 
 
 # Настройки
